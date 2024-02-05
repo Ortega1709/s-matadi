749fabeb9899adedcae028234ff43e194049d3cf
ortega <kabweortega@gmail.com>
2024-02-05 08:29:02 +0200

    Build: Add spring cloud routing dependency

--- src/main/java/com/ortega/smatadi/SMatadiController.java
+++ src/main/java/com/ortega/smatadi/SMatadiController.java
@@ -1,4 +1,4 @@
-package com.ortega.smatadi.controllers;
+package com.ortega.smatadi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
